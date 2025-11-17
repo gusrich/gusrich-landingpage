7117c2916a76a49448e3651de90136e701c61efb
Gusrich Programmer <gus.isrich999@gmail.com>
2025-11-17 17:43:39 +0700

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,23 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: "https://gusrich-landingpage.vercel.app/#about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://gusrich-landingpage.vercel.app/#services",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://gusrich-landingpage.vercel.app/#portfolio",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
   ];
 }
